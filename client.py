748d296014462ac4ff7da51f52cdbeb16a86a3be
Lars Larsson <larsson@cs.umu.se>
2020-01-21 07:53:18 +0100

    Corrected behavior

--- client.py
+++ client.py
@@ -2,32 +2,33 @@
 
 import grpc
 
-import value_pb2 as pb
-import value_pb2_grpc as stub
+import value_pb2
+import value_pb2_grpc
 
 import logging
 import random
 import time
-
-def should_update_value():
-    return False
+import os
 
 def delay():
     time.sleep(1)
 
-def run(port):
-    with grpc.insecure_channel("localhost:{}".format(port)) as channel:
-        service = stub.ValueServiceStub(channel)
-        correct = service.GetValue(pb.Empty()).value
+def run(addr, mean):
+    with grpc.insecure_channel(addr) as channel:
+        service = value_pb2_grpc.ValueServiceStub(channel)
         while (True):
-            if should_update_value():
-                correct = random.randint(1, 100000000)
-                service.SetValue(pb.Value(value=correct))
+            correct = random.randint(1, 100000000)
+            service.SetValue(value_pb2.Value(value=correct))
             delay()
-            actual = service.GetValue(pb.Empty()).value
-            logging.info("%d,%d", correct, actual)
+
+            requests = int(random.expovariate(1/mean))
+            while requests:
+                actual = service.GetValue(value_pb2.Empty()).value
+                logging.info("%d,%d", correct, actual)
+                delay()
+                requests -= 1
 
 if __name__=="__main__":
-    random.seed(10)
+    random.seed(int(os.getenv("SEED", 42)))
     logging.basicConfig(format='%(asctime)s,%(message)s', datefmt='%s', level=logging.INFO)
-    run(8080)
+    run(os.getenv("VALUE_SERVICE_ADDR", "localhost:1111"), os.getenv("MEAN", 20.0))
--- server.py
+++ server.py
@@ -3,32 +3,35 @@
 import grpc
 from concurrent import futures
 
-import value_pb2 as pb
-import value_pb2_grpc as stub
+import value_pb2
+import value_pb2_grpc
 
 import logging
+import os
 
-class ValueServiceServicer(stub.ValueServiceServicer):
+class ValueServiceServicer(value_pb2_grpc.ValueServiceServicer):
     def __init__(self):
         self.value = 1
 
     def GetValue(self, request, context):
-        result = pb.Value(value=self.value)
-        logging.info("get,%d", result.value)
+        result = value_pb2.Value()
+        result.value = self.value
+        logging.info("get,%d", self.value)
         return result
 
     def SetValue(self, request, context):
-        self.value = request.value.value
+        self.value = request.value
         logging.info("set,%d", self.value)
+        return value_pb2.Empty()
 
 def serve(port):
     # single-threaded for dependable results
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-    stub.add_ValueServiceServicer_to_server(ValueServiceServicer(), server)
+    value_pb2_grpc.add_ValueServiceServicer_to_server(ValueServiceServicer(), server)
     server.add_insecure_port("[::]:{}".format(port))
     server.start()
     server.wait_for_termination()
 
 if __name__=="__main__":
     logging.basicConfig(format='%(asctime)s,%(message)s', datefmt='%s', level=logging.INFO)
-    serve(8080)
+    serve(int(os.getenv("PORT", 1111)))
