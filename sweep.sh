5b71212a043489fa3a71c0f52066d85dddfef384
Lars Larsson <larsson@cs.umu.se>
2020-01-24 14:32:41 +0100

    Better rates

--- sweep.sh
+++ sweep.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 for repetition in $(seq 3); do
 	for proxy_max_age in 0 1 10 30 dynamic; do
-		for get_rate in 1 2 3 4 5 6 7 8 9 10; do
-			for set_rate in 1 2 3 4 5 6 7 8 9 10; do
+		for get_rate in 100 50 10 5 1; do
+			for set_rate in 0.01 0.02 0.1 0.2 1; do
 				experiment_id="${proxy_max_age}-maxage-${get_rate}-getrate-${set_rate}-setrate-${repetition}-repetition"
 				echo "${experiment_id}"
 				./run-experiment.sh ${experiment_id} ${get_rate} ${set_rate} ${proxy_max_age}
