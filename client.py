c5df2dcfe75baf79719acf65600662ca2ef79b2c
Lars Larsson <larsson@cs.umu.se>
2020-01-24 12:03:08 +0100

    Dockerization

--- client.py
+++ client.py
@@ -13,24 +13,30 @@ import os
 def delay():
     time.sleep(1)
 
-def run(addr, mean):
+def run(addr, mean, max_count):
     with grpc.insecure_channel(addr) as channel:
         service = value_pb2_grpc.ValueServiceStub(channel)
         correct = 0
-        while (True):
+        count = 0
+        while count <= max_count:
+            count += 1
             correct += 1
             service.SetValue(value_pb2.Value(value=correct))
             delay()
 
             requests = int(random.expovariate(1/mean)) + 1
-            while requests:
+            while requests and count <= max_count:
+                count += 1
                 actual = service.GetValue(value_pb2.Empty()).value
-                logging.info("%d,%d", correct, actual)
+                logging.info("%d,%d,%d", time.time_ns(), correct, actual)
                 requests -= 1
                 if requests:
                     delay()
 
 if __name__=="__main__":
     random.seed(int(os.getenv("SEED", 42)))
-    logging.basicConfig(format='%(asctime)s,%(message)s', datefmt='%s', level=logging.INFO)
-    run(os.getenv("VALUE_SERVICE_ADDR", "localhost:1100"), os.getenv("MEAN", 20.0))
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.info("timestamp,correct,actual")
+    run(os.getenv("VALUE_SERVICE_ADDR", "localhost:1100"), 
+            float(os.getenv("MEAN", 20.0)),
+            int(os.getenv("COUNT", 300)))
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import value_pb2_grpc
 
 import logging
 import os
+import time
 
 class ValueServiceServicer(value_pb2_grpc.ValueServiceServicer):
     def __init__(self):
@@ -16,12 +17,12 @@ class ValueServiceServicer(value_pb2_grpc.ValueServiceServicer):
     def GetValue(self, request, context):
         result = value_pb2.Value()
         result.value = self.value
-        logging.info("get,%d", self.value)
+        logging.info("%d,get,%d", time.time_ns(), self.value)
         return result
 
     def SetValue(self, request, context):
         self.value = request.value
-        logging.info("set,%d", self.value)
+        logging.info("%d,set,%d", time.time_ns(), self.value)
         return value_pb2.Empty()
 
 def serve(port):
@@ -33,5 +34,6 @@ def serve(port):
     server.wait_for_termination()
 
 if __name__=="__main__":
-    logging.basicConfig(format='%(asctime)s,%(message)s', datefmt='%s', level=logging.INFO)
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.info("timestamp,operation,value")
     serve(int(os.getenv("PORT", 1100)))
