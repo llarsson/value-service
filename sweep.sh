11bd0223607a9285ba55510a4445d35736a24946
Lars Larsson <larsson@cs.umu.se>
2020-01-31 08:23:06 +0100

    set_rate=10 added

--- sweep.sh
+++ sweep.sh
@@ -10,14 +10,10 @@ rm ${sweepfile} || true
 for repetition in $(seq 3); do
 	for proxy_max_age in static-0 static-1 static-10 static-30 dynamic-simplistic dynamic-tbg1 dynamic-nyqvistish; do
 		for get_rate in 100 50 25 10 5 1 0.2; do
-			for set_rate in 0.01 0.02 0.04 0.1 0.2 0.5 1 5; do
+			for set_rate in 0.01 0.02 0.04 0.1 0.2 0.5 1 5 10; do
 				experiment_id="${proxy_max_age}-maxage-${get_rate}-getrate-${set_rate}-setrate-${repetition}-repetition"
 				export duration=300
 				export seed=${repetition}
-				#until ./run-experiment.sh ${experiment_id} ${get_rate} ${set_rate} ${proxy_max_age}; do
-				#	echo "Some error occurred, sleeping and trying again..."
-				#	sleep 5
-				#done
 				echo "duration=${duration} seed=${seed} ./run-experiment.sh ${experiment_id} ${get_rate} ${set_rate} ${proxy_max_age}" >> ${sweepfile}
 			done
 		done
