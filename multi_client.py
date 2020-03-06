b932244c4f2478f74853b68db0e4eb84c86c5fa4
Lars Larsson <larsson@cs.umu.se>
2020-03-06 11:22:38 +0100

    Possible to choose delay function

--- multi_client.py
+++ multi_client.py
@@ -8,13 +8,14 @@ import value_pb2
 import value_pb2_grpc
 
 import logging
+import math
 import random
 import time
 import os
 
 from multiprocessing import Process, Value
 
-def setter(addr, rate, end_time, correct):
+def setter(addr, end_time, correct, delay_function):
     random.seed(int(os.getenv("SEED", 42)))
     delay_sum = 0
     delays = 0
@@ -24,7 +25,7 @@ def setter(addr, rate, end_time, correct):
             req_start = time.time()
             service.SetValue(value_pb2.Value(value=correct.value))
             req_end = time.time()
-            delay = random.expovariate(rate) - (req_start - req_end)
+            delay = delay_function(req_end) - (req_start - req_end)
             time.sleep(delay)
             correct.value += 1
             delay_sum += delay
@@ -35,7 +36,7 @@ def setter(addr, rate, end_time, correct):
        stats.write("{},{},{}\n".format(delays, delay_sum, delay_sum / delays))
 
 
-def getter(addr, rate, end_time, correct):
+def getter(addr, end_time, correct, delay_function):
     random.seed(int(os.getenv("SEED", 42)))
     delay_sum = 0
     delays = 0
@@ -58,7 +59,7 @@ def getter(addr, rate, end_time, correct):
                 delays += 1
                 # delay must be proportional to the time it took to make request
                 work_end = time.time()
-                delay = random.expovariate(rate) - (work_start - work_end)
+                delay = delay_function(work_end) - (work_start - work_end)
                 delay_sum += delay
                 time.sleep(delay)
             except:
@@ -69,19 +70,61 @@ def getter(addr, rate, end_time, correct):
        stats.write("delays,delay_sum,mean\n") 
        stats.write("{},{},{}\n".format(delays, delay_sum, delay_sum / delays))
 
+def poisson(rate):
+    def f(time):
+        return random.expovariate(rate)
+    return f
+
+def sinusodial(min_rate, max_rate, period, phase):
+    b = min_rate
+    a = (max_rate - b) / 2.0
+    def f(time):
+        y = a + b + a * math.sin(time * 2 * math.pi / period + phase)
+        return random.expovariate(y)
+    return f
 
 if __name__ == "__main__":
     correct = Value('I', 1)
 
     addr = os.getenv("VALUE_SERVICE_ADDR", "localhost:1100")
-    set_rate = float(os.getenv("SET_RATE", 20.0))
-    get_rate = float(os.getenv("GET_RATE", 20.0))
     end_time = time.time() + float(os.getenv("DURATION", 300))
 
-    set_process = Process(target=setter, args=(addr, set_rate, end_time, correct))
+    get_mode = os.getenv("GETTER_MODE", "poisson")
+    set_mode = os.getenv("SETTER_MODE", "poisson")
+
+    getter_poisson_rate = float(os.getenv("GETTER_POISSON_RATE", 20.0))
+    setter_poisson_rate = float(os.getenv("SETTER_POISSON_RATE", 20.0))
+
+    getter_sinusodial_min_rate = float(os.getenv("GETTER_SINUSODIAL_MIN_RATE", 1))
+    getter_sinusodial_max_rate = float(os.getenv("GETTER_SINUSODIAL_MAX_RATE", 10))
+    getter_sinusodial_period = float(os.getenv("GETTER_SINUSODIAL_PERIOD", 1200))
+    getter_sinusodial_phase = float(os.getenv("GETTER_SINUSODIAL_PHASE", 0))
+
+    setter_sinusodial_min_rate = float(os.getenv("SETTER_SINUSODIAL_MIN_RATE", 0.05))
+    setter_sinusodial_max_rate = float(os.getenv("SETTER_SINUSODIAL_MAX_RATE", 1))
+    setter_sinusodial_period = float(os.getenv("SETTER_SINUSODIAL_PERIOD", 1200))
+    setter_sinusodial_phase = float(os.getenv("SETTER_SINUSODIAL_PHASE", 0))
+
+    getter_delay_function = None
+    if get_mode == "sinusodial":
+        getter_delay_function = sinusodial(getter_sinusodial_min_rate, 
+                getter_sinusodial_max_rate, getter_sinusodial_period,
+                getter_sinusodial_phase)
+    else:
+        getter_delay_function = poisson(getter_poisson_rate)
+
+    setter_delay_function = None
+    if set_mode == "sinusodial":
+        setter_delay_function = sinusodial(setter_sinusodial_min_rate, 
+                setter_sinusodial_max_rate, setter_sinusodial_period,
+                setter_sinusodial_phase)
+    else:
+        setter_delay_function = poisson(setter_poisson_rate)
+
+    set_process = Process(target=setter, args=(addr, end_time, correct, setter_delay_function))
     set_process.start()
 
-    get_process = Process(target=getter, args=(addr, get_rate, end_time, correct))
+    get_process = Process(target=getter, args=(addr, end_time, correct, getter_delay_function))
     get_process.start()
 
     set_process.join()
