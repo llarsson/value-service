2ae9c266ead2b3f2e3e22d1239e669490973c40c
Lars Larsson <lars.larsson@elastisys.com>
2020-03-13 12:55:39 +0100

    Just use the algorithms from literature and static one

--- sinusodial-sweep.sh
+++ sinusodial-sweep.sh
@@ -8,7 +8,7 @@ sweepfile=sweep.joblist
 rm ${sweepfile} || true
 
 for repetition in $(seq 3); do
-	for proxy_max_age in static-0 static-1 static-10 static-30 dynamic-simplistic dynamic-tbg1 dynamic-adaptive-0.1 dynamic-adaptive-0.25 dynamic-adaptive-0.5 dynamic-updaterisk-0.1 dynamic-updaterisk-0.25 dynamic-updaterisk-0.50 dynamic-updaterisk-0.75 dynamic-updaterisk-0.90; do
+	for proxy_max_age in static-0 static-1 static-10 static-30 dynamic-adaptive-0.1 dynamic-adaptive-0.25 dynamic-adaptive-0.5 dynamic-updaterisk-0.1 dynamic-updaterisk-0.25 dynamic-updaterisk-0.50 dynamic-updaterisk-0.75 dynamic-updaterisk-0.90; do
 		for setter_phase in -5454.5 450 900 1800 ; do
 		 	experiment_id="${proxy_max_age}-maxage-${setter_phase}-setter_phase-${repetition}-repetition"
 		 	export duration=1800
