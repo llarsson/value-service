a6fafa87522a7e06d0bc52488bae48ef63a3632a
Lars Larsson <larsson@cs.umu.se>
2020-03-13 10:10:32 +0100

    Human-readable seconds in epoch timestamp

--- sinusodial-plot.py
+++ sinusodial-plot.py
@@ -109,7 +109,7 @@ def binned_stats(experiment, period=60):
     result['mean_work_fraction'] = binned_work_reduction['fraction']
     result['mean_error_fraction'] = binned_client['errors']
 
-    result['epoch_timestamp'] = result.index.astype('int64')
+    result['epoch_timestamp'] = result.index.astype('int64') / 1000000000
 
     return result
 
