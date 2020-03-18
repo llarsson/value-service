7eb64e041c434f9a289d112acba37b07bac23731
Lars Larsson <larsson@cs.umu.se>
2020-03-18 09:14:10 +0100

    Working multi-radar chart

--- radar_chart.py
+++ radar_chart.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+# Thanks to https://python-graph-gallery.com/390-basic-radar-chart/
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from math import pi
 
 import sys
 
-def radar_chart(df, item=0):
+category_labels = {'traffic_reduction': 'Traffic reduction',
+        'server_work_fraction': 'Work fraction',
+        'error_fraction': 'Error fraction'}
+
+def radar_chart(df, indices, title=''):
     # configure axis {{{
     # number of variable
     categories=list(df.columns)
@@ -20,31 +26,29 @@ def radar_chart(df, item=0):
     ax = plt.subplot(111, polar=True)
     
     # Draw one axis per variable + add labels labels yet
-    plt.xticks(angles[:-1], categories, color='grey', size=8)
+    plt.xticks(angles[:-1], [category_labels[category] for category in categories], color='black', size=8)
+    plt.tick_params(axis='x', pad=20)
     
     # Draw ylabels
-    ax.set_rlabel_position(0)
-    plt.yticks([n / 10 for n in range(1, 12, 3)], color="grey", size=7)
-    plt.ylim(0,1.2)
+    ax.set_rmax(1.2)
+    ax.set_ylim(1.2)
+    ax.set_rlabel_position(-60)
+    ax.set_rticks([0.0, 0.25, 0.5, 1.0], ['', '0.25', '0.50', '1.00'])
+    #plt.yticks([n / 10 for n in range(1, 12, 3)], color="grey", size=7)
+    #plt.ylim(0,1.2)
     # }}}
 
-    # Pick data to plot
-    element = None
-    if isinstance(item, int):
-        element=df.iloc[item]
-    else:
-        element=df.loc[item]
+    for element in [df.loc[index] for index in indices]:
+        # We need to repeat the first value to close the circular graph:
+        values=element.values.flatten().tolist()
+        values += values[:1]
+        
+        # Plot data
+        ax.plot(angles, values, linewidth=1, linestyle='solid', label=element.name)
+        ax.fill(angles, values, alpha=0.1)
 
-    # We need to repeat the first value to close the circular graph:
-    values=element.values.flatten().tolist()
-    values += values[:1]
-    
-    # Plot data
-    ax.plot(angles, values, linewidth=1, linestyle='solid', label=element.name)
-    ax.fill(angles, values, alpha=0.1)
-
-    # FIXME make pretty
-    plt.legend()#)loc='upper right')#, bbox_to_anchor=(0.1, 0.1))
+    plt.legend(loc='upper right', bbox_to_anchor=(1.35, 1.15))
+    plt.suptitle(title)
     plt.show()
 
 if __name__=='__main__':
@@ -52,5 +56,5 @@ if __name__=='__main__':
     summary.set_index(['algorithm', 'phase'], inplace=True)
     summary.drop('goodness', axis=1, inplace=True)
     print(summary.index)
-    radar_chart(summary, ('dynamic-updaterisk-0.50', 900.0))
+    radar_chart(summary, [('dynamic-updaterisk-0.50', -5454.5), ('dynamic-updaterisk-0.50', 900.0), ('dynamic-adaptive-0.5', 900)], title='foo title')
 
