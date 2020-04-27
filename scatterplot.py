7d55b863146b2a26ee29e7269780cb18819de3eb
Lars Larsson <larsson@cs.umu.se>
2020-04-27 08:34:56 +0200

    Semi-transparent markers

--- scatterplot.py
+++ scatterplot.py
@@ -11,9 +11,9 @@ def scatterplot(summary):
     adaptive = summary.query('name == "dynamic-adaptive"')
     updaterisk = summary.query('name == "dynamic-updaterisk"')
 
-    plt.scatter(static['error_fraction'], static['traffic_reduction'], s=static['parameter'] / 30.0 * 200, marker='o',  label='static', facecolor='white', edgecolor='black')
-    plt.scatter(adaptive['error_fraction'], adaptive['traffic_reduction'], s=adaptive['parameter'] / 0.5 * 200, marker='D',  label='dynamic-adaptive', facecolor='white', edgecolor='black')
-    plt.scatter(updaterisk['error_fraction'], updaterisk['traffic_reduction'], s=updaterisk['parameter'] / 0.9 * 200, marker='s', label='dynamic-updaterisk', facecolor='white', edgecolor='black')
+    plt.scatter(static['error_fraction'], static['traffic_reduction'], s=static['parameter'] / 30.0 * 200, marker='o',  label='static', facecolor='white', edgecolor='black', alpha=0.5)
+    plt.scatter(adaptive['error_fraction'], adaptive['traffic_reduction'], s=adaptive['parameter'] / 0.5 * 200, marker='D',  label='dynamic-adaptive', facecolor='white', edgecolor='black', alpha=0.5)
+    plt.scatter(updaterisk['error_fraction'], updaterisk['traffic_reduction'], s=updaterisk['parameter'] / 0.9 * 200, marker='s', label='dynamic-updaterisk', facecolor='white', edgecolor='black', alpha=0.5)
 
     plt.xlabel('Error fraction')
     plt.ylabel('Traffic reduction')
